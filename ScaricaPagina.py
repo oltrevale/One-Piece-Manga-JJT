562c47b58906cd342de3b9e76b2924cf465de55f
oltrevale <37437806+oltrevale@users.noreply.github.com>
2021-01-06 20:46:28 +0100

    pdf

--- ScaricaPagina.py
+++ ScaricaPagina.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import time
+from PIL import Image
 
-
+lista = []
 def ScaricaPagina(Pagina, Capitolo):
     Pagina = str(Pagina)
     Capitolo = str(Capitolo)
     try:
-        os.mkdir(f"C:\\Users\\K\\Desktop\\{Capitolo}")
+        os.mkdir(f"C:\\Users\\Kappa\\Desktop\\{Capitolo}")
     except OSError:
         pass
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -18,15 +19,21 @@ def ScaricaPagina(Pagina, Capitolo):
     immagine = soup.find( class_="open open_image")
     link = immagine["src"]
     link = link.replace(" ", "")
-    with open(f"C:\\Users\\K\\Desktop\\{Capitolo}\\{Pagina}.png", "wb") as f:
+    with open(f"C:\\Users\\Kappa\\Desktop\\{Capitolo}\\{Pagina}.png", "wb") as f:
         f.write(requests.get(link).content)
+    image = Image.open(f'C:\\Users\\Kappa\\Desktop\\{Capitolo}\\{Pagina}.png')
+    image = image.convert('RGB')
+    lista.append(image)
     print(f"la pagina numero {Pagina} del capitolo numero {Capitolo} è stata scaricata")
+    print(link)
+    return image
 
 
 def ScaricaCapitolo(Capitolo):
     for Pagina in range(1, 23):
-        ScaricaPagina(Pagina, Capitolo)
+        image = ScaricaPagina(Pagina, Capitolo)
         time.sleep(3)
+    image.save(f'C:\\Users\\Kappa\\Desktop\\{Capitolo}.pdf',save_all=True, append_images=lista)
 
 if __name__ == "__main__":
     ScaricaCapitolo(912)
