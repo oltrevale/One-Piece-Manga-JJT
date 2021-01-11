951c68ccd05d4163bc6722c07f9ebacdbb5ad486
oltrevale <37437806+oltrevale@users.noreply.github.com>
2021-01-11 17:55:25 +0100

    fix

---
+++ scarica_pagina.py
@@ -0,0 +1,46 @@
+import os
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+lista = []
+
+
+def scarica_pagina(pagina, capitolo):
+    pagina = str(pagina)
+    capitolo = str(capitolo)
+    try:
+        os.mkdir(f"C:\\Users\\K\\Desktop\\{capitolo}")
+    except OSError:
+        pass
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/39.0.2171.95 Safari/537.36'}
+    link = f"https://www.juinjutsureader.ovh/read/one-piece/it/0/{capitolo}/page/{pagina}"
+    risposta = requests.get(link, headers=headers)
+    soup = BeautifulSoup(risposta.text, "html.parser")
+    immagine = soup.find(class_="open open_image")
+    link = immagine["src"]
+    link = link.replace(" ", "")
+    with open(f"C:\\Users\\K\\Desktop\\{capitolo}\\{pagina}.png", "wb") as f:
+        f.write(requests.get(link).content)
+    print(f"la pagina numero {pagina} del capitolo numero {capitolo} è stata scaricata")
+    print(link)
+    return link
+
+
+def scarica_capitolo(capitolo):
+    lista_link = []
+    for pagina in range(1, 23):
+        link = scarica_pagina(pagina, capitolo)
+        if link in lista_link:
+            pass
+        else:
+            lista_link.append(link)
+    time.sleep(1)
+    return len(lista_link)
+
+
+if __name__ == "__main__":
+    scarica_capitolo(912)
