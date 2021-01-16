597cbf8bb411394dfea2fdd8fbfa23963d116103
K <valeriocampanella1998@gmai.com>
2021-01-16 12:30:09 +0100

    fix ultima pagina come prima e creato algoritmo barbaro per rimuovere pagine

--- archivia.py
+++ archivia.py
@@ -6,7 +6,7 @@ logging.basicConfig(filename='ciao.log', level=logging.INFO)
 
 def crea_pdf_capitolo(lunghezza_lista, capitolo):
     lista = []
-    for pagina in range(1, lunghezza_lista + 1):
+    for pagina in range(1, lunghezza_lista):
         image = Image.open(f'C:\\Users\\K\\Desktop\\{capitolo}\\{pagina}.png')
         image = image.convert('RGB')
         lista.append(image)
@@ -14,17 +14,15 @@ def crea_pdf_capitolo(lunghezza_lista, capitolo):
 
 
 def crea_pdf_capitoli(capitolo_inizio, capitolo_fine, dizionario):
-    # FIXME utima pagina diventa prima nel pdf
-    i = 0
     lista = []
     for capitolo in range(capitolo_inizio, capitolo_fine + 1):
         for pagina in range(1, dizionario[capitolo]):
             image = Image.open(f'c:\\Users\\K\\Desktop\\{capitolo}\\{pagina}.png')
             image = image.convert('RGB')
-            if image.size == (3385, 2560) or image.size == (1390, 2100):
-                lista.append(image)
+            lista.append(image)
     logging.info(len(lista))
     for elemento in lista:
         logging.info(elemento)
         logging.info('\n')
-    image.save(f'C:\\Users\\K\\Desktop\\{capitolo_inizio}-{capitolo_fine}.pdf', save_all=True, append_images=lista)
+    image = lista[0]
+    image.save(f'C:\\Users\\K\\Desktop\\{capitolo_inizio}-{capitolo_fine}.pdf', save_all=True, append_images=lista[1:])
--- scarica_pagina.py
+++ scarica_pagina.py
@@ -1,43 +1,59 @@
 import os
-
+from PIL import Image
 import requests
 from bs4 import BeautifulSoup
+import logging
 
+logging.basicConfig(level=logging.INFO, filename='came.log')
 lista = []
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scarica_pagina(pagina, capitolo):
-    pagina = str(pagina)
+def recupera_link(capitolo, pagina):
     capitolo = str(capitolo)
-    try:
-        os.mkdir(f"C:\\Users\\K\\Desktop\\{capitolo}")
-    except OSError:
-        pass
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/39.0.2171.95 Safari/537.36'}
+    pagina = str(pagina)
+
     link = f"https://www.juinjutsureader.ovh/read/one-piece/it/0/{capitolo}/page/{pagina}"
     risposta = requests.get(link, headers=headers)
     soup = BeautifulSoup(risposta.text, "html.parser")
     immagine = soup.find(class_="open open_image")
     link = immagine["src"]
     link = link.replace(" ", "")
-    with open(f"C:\\Users\\K\\Desktop\\{capitolo}\\{pagina}.png", "wb") as f:
-        f.write(requests.get(link).content)
-    print(f"la pagina numero {pagina} del capitolo numero {capitolo} è stata scaricata")
-    print(link)
     return link
 
 
 def scarica_capitolo(capitolo):
-    lista_link = []
+    indice = 1
+    try:
+        os.mkdir(f"C:\\Users\\K\\Desktop\\{capitolo}")
+    except OSError:
+        pass
+    dizionario = {}
     for pagina in range(1, 23):
-        link = scarica_pagina(pagina, capitolo)
-        if link in lista_link:
+        link = recupera_link(capitolo, pagina)
+        if link in dizionario.keys():
             pass
         else:
-            lista_link.append(link)
-    return len(lista_link)
+            dizionario[link] = pagina
+    print(dizionario.values)
+    for link in dizionario.keys():
+        with open(f"C:\\Users\\K\\Desktop\\{capitolo}\\prova.png", "wb") as f:
+            f.write(requests.get(link, headers=headers).content)
+        image = Image.open(f'C:\\Users\\K\\Desktop\\{capitolo}\\prova.png')
+        image = image.convert('RGB')
+        if image.size == (3385, 2560) or image.size == (1390, 2100):
+            image.close()
+            os.rename(f"C:\\Users\\K\\Desktop\\{capitolo}\\prova.png",
+                      f"C:\\Users\\K\\Desktop\\{capitolo}\\{indice}.png")
+            indice += 1
+            logging.info(indice)
+            logging.info('\n')
+
+        else:
+            pass
+    return indice
 
 
 if __name__ == "__main__":
